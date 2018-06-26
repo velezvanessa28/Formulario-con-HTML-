0c03a77789081e59ce8fbad2dcce97e2c5a15322
velezvanessa28 <velezvanessa193@gmail.com>
2018-06-25 19:57:17 -0500

    cambios

---
+++ main.js
@@ -0,0 +1,38 @@
+var fecha_solicitud = "27-10-2018";
+
+var nombre = "Pepito Perez";
+
+var fecha_nacimiento = "27-06-1996";
+
+var correo = "alguien@ejemplo.com";
+
+var telefono = "5557776";
+
+var genero = "Masculino";
+
+var Departamento = "Antioquia";
+
+var Ciudad = "Medellin";
+
+var direccion = "Cra 22 #22c -22";
+
+var domicilio = "Casa";
+
+var domicilio_otro = "";
+
+var integrantes = 2;
+
+var adoptar_mascota = "Porque no quiero estar solo";
+
+var experiencia = true;
+
+var otras_mascotas = true;
+
+var ingresos = 500000;
+
+var ingresos_mascotas = 200000;
+
+var tiempo = 4;
+
+var ausente = "Mi mamita";
+
